853297e9730b8ff481d9f4b86386ab415ff4b946
calumpetergunn <calumgunn@gmail.com>
2021-04-10 17:13:34 +0100

    connected to API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Welcome!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data () {
+    return {
+      artworksList: []
+    }
+  },
+
+  async mounted() {
+    const promises = [500955, 500956, 500957, 500958, 500959, 500960, 500961, 500962, 500963, 500964, 500965, 500967, 500968, 500967, 500968, 500969, 500970, 500971, 500972, 500973, 500974, 500975, 500976, 500977, 500978, 500979, 500980, 500981, 500982, 500983, 500984].map(async (number) => {
+      const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${number}`)
+      return await response.json()
+    })
+    const nestedArtWorks = await Promise.all(promises)
+    this.artworksList = nestedArtWorks.flat()
+    console.log(this.artworksList)
   }
 }
 </script>
